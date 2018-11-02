5de957a4d589d6328b6a89a3a6f71d1e757bd60f
jesus lima <jesus_manuel@outlook.com>
2018-11-02 17:14:44 -0400

    Added string extension[convert string to number]

---
+++ src/JLI.Extension/StringExtension.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Linq;
+
+namespace JLI.Extension
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Convert a string to Int, float, double.
+        /// </summary>
+        /// <typeparam name="T">type of date.</typeparam>
+        /// <param name="source">string to convert.</param>
+        /// <returns>a number.</returns>
+        public static T ToNumber<T>(this string source) where T : struct
+        {
+            if (typeof(T) != typeof(Int16) 
+                && typeof(T) != typeof(Int32) 
+                && typeof(T) != typeof(Int64) 
+                && typeof(T) != typeof(UInt16)
+                && typeof(T) != typeof(UInt32)
+                && typeof(T) != typeof(UInt64)
+                && typeof(T) != typeof(Single)
+                && typeof(T) != typeof(float)
+                && typeof(T) != typeof(decimal)
+                && typeof(T) != typeof(double))
+            {
+                throw new ArgumentException(
+                  string.Format("Type '{0}' is not valid.", typeof(T).ToString()));
+            }
+
+            return (T)Convert.ChangeType(source, typeof(T));
+        }
+
+        /// <summary>
+        /// Check if the string is a number.
+        /// </summary>
+        /// <param name="source">string to check.</param>
+        /// <returns>true if string is number otherwise false.</returns>
+        public static bool IsNumber(this string source)
+        {
+            if(string.IsNullOrEmpty(source))
+            {
+                return false;
+            }
+
+            var cleanString = source.Where(at=> at != '.' && at != '-');
+            return   (cleanString.All(char.IsDigit) || cleanString.All(char.IsNumber));
+        }
+    }
+}
---
+++ src/UnitTest/StringExtensionTest.cs
@@ -0,0 +1,60 @@
+﻿
+using JLI.Extension;
+using NUnit.Framework;
+using System;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class StringExtensionTest
+    {
+        private const int INT_INTEGER = 112;
+
+        private const double DOUBLE = 123.12;
+
+        private const float FLOAT = -123.4561F;
+
+        [Test]
+        public void Given_Type_Of_Int_And_Valid_String_Should_Convert_To_Int()
+        {
+            var number = INT_INTEGER.ToString().ToNumber<int>();
+            Assert.IsTrue(number.GetType()== typeof(int) && number == INT_INTEGER);
+        }
+
+        [Test]
+        public void Given_Type_Of_Bool_And_Valid_String_Should_Throw_Exception()
+        {
+            Assert.Catch(() => { INT_INTEGER.ToString().ToNumber<bool>(); });
+        }
+
+        [Test]
+        public void Given_Type_Of_Double_And_Valid_String_Should_Convert_To_Double()
+        {
+            var number = DOUBLE.ToString().ToNumber<double>();
+            Assert.IsTrue(number.GetType() == typeof(double) && number == DOUBLE);
+        }
+
+        [Test]
+        public void Given_Type_Of_Float_And_Valid_String_Should_Convert_To_Float()
+        {
+            var number = FLOAT.ToString().ToNumber<float>();
+            Assert.IsTrue(number == FLOAT);
+        }
+
+        [TestCase("123", true)]
+        [TestCase("-123", true)]
+        [TestCase("123.123", true)]
+        [TestCase("-123.123", true)]
+        [TestCase("0.123", true)]
+        [TestCase("-0.123", true)]
+        [TestCase("0.123tr", false)]
+        [TestCase("", false)]
+        [TestCase("abc", false)]
+        public void Given_String_Should_Know_If_String_Is_Number(string source, bool expected)
+        {
+            var t = source.IsNumber();
+            Assert.IsTrue(t == expected);
+        }
+
+    }
+}
